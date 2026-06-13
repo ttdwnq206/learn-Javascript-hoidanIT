b6c6bebc0ee09ae744790cb64102fdad0025c55d
Triệu Tài Dũng <trieutaidung0506@gmail.com>
2026-06-13 15:16:54 +0700

    video17+18

---
+++ src/variables/video17.js
@@ -0,0 +1,18 @@
+console.log("Video 17");
+
+//var là cách khai báo cũ
+var name = "dungsenpai";
+var name1 = "name1";
+console.log(name);
+
+//let khai báo biến, biến này có thể đổi giá trị
+let age = 20;
+console.log(age);
+age = 25;
+console.log(age);
+
+//const là hằng số
+const birthday = 1505;
+
+//var mềm dẻo ở phạm vi sử dụng biến hơn let
+//var có phạm vi sử dụng biến rộng rãi nhất
---
+++ src/variables/video18.js
@@ -0,0 +1,27 @@
+console.log("video18");
+
+//number
+const myAge = 25;
+const myScore = 9.1;
+
+console.log(myAge);
+console.log(myScore);
+
+//string
+const name = "ttdung";
+const name1 = "hello"; //tương đương: 'hello'
+const name2 = `dung`;
+
+//boolean: true/false
+const isSenpai = true;
+console.log(isSenpai);
+
+//không gán giá trị thì biến sẽ là undefined
+//undefined: chưa định nghĩa
+//
+let bla;
+console.log(bla);
+
+//null: biểu thị sự "không có giá trị" 1 cách cố ý
+let testNull = null;
+console.log(testNull);
